795145cd9d2b96832a85cd0a82ebdc42dbcd708e
Zach King <kingzach77@gmail.com>
2015-09-28 17:57:19 -0500

    Gold coins here too

---
+++ 2015-09-28/knight-coins.py
@@ -0,0 +1,33 @@
+'''
+File: knight-coins.py
+Author: Zachary King
+Description: Knight's coins ACM practice problem
+'''
+
+def getRich(n):
+	days = list(range(n))
+	pay, days_until_pay_increase = 1, 1
+	total = 0
+	for day in days:
+		total += pay
+		days_until_pay_increase -= 1
+		if days_until_pay_increase == 0:
+			pay += 1
+			days_until_pay_increase = pay
+	return total
+
+
+def main():
+	input_f = open("GoldCoins.in", 'r')
+	output_f = open("GoldCoins.out", 'w')
+
+	for n in input_f:
+		n = int(n)
+		if n == 0: break
+		output_f.write(str(getRich(n)) + "\n")
+
+	input_f.close()
+	output_f.close()
+
+if __name__ == "__main__":
+	main()
